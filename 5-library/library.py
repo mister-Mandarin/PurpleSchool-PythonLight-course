9541bbe4def1c3eb4771a08fb8b9347a518b9b0e
Mandarin <andr.poluehktov@yandex.ru>
2025-12-21 20:34:27 +0300

    ДЗ библиотека 2 fix

--- 5-library/library.py
+++ 5-library/library.py
@@ -1,11 +1,11 @@
 import sys
 
 BOOKS = [
-    {"title": "Война и мир", "author": "Лев Толстой"},
-    {"title": "Преступление и наказание", "author": "Фёдор Достоевский"},
-    {"title": "Мастер и Маргарита", "author": "Михаил Булгаков"},
-    {"title": "Анна Каренина", "author": "Лев Толстой"},
-    {"title": "Герой нашего времени", "author": "Михаил Лермонтов"}
+    {"book": "Война и мир", "author": "Лев Толстой"},
+    {"book": "Преступление и наказание", "author": "Фёдор Достоевский"},
+    {"book": "Мастер и Маргарита", "author": "Михаил Булгаков"},
+    {"book": "Анна Каренина", "author": "Лев Толстой"},
+    {"book": "Герой нашего времени", "author": "Михаил Лермонтов"}
 ]
 # python library.py sort author
 # python library.py filter "Толстой"
@@ -13,16 +13,24 @@ BOOKS = [
 action = sys.argv[1]
 param = sys.argv[2]
 
+
+def print_list_books(data: list[dict]):
+    return list(map(lambda x: f"{x['book']} - {x['author']}", data))
+
+
 match action:
     case 'sort':
         match param:
             case 'author':
-                print(sorted(BOOKS, key=lambda x: x['author']))
-            case 'title':
-                print(sorted(BOOKS, key=lambda x: x['title']))
+                sorted_by_author = sorted(BOOKS, key=lambda x: x['author'])
+                print(print_list_books(sorted_by_author))
+            case 'book':
+                sorted_by_book = sorted(BOOKS, key=lambda x: x['book'])
+                print(print_list_books(sorted_by_book))
             case _:
                 print('Некорректный ввод!')
     case 'filter':
-        print(list(filter(lambda x: param in x['author'], BOOKS)))
+        filtered = list(filter(lambda x: param in x['author'], BOOKS))
+        print(print_list_books(filtered))
     case _:
         print('Некорректный ввод!')
