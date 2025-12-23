f3d92edf634be9991bb9d8068905d2e55111bfbc
Mandarin <andr.poluehktov@yandex.ru>
2025-12-23 12:18:05 +0300

    change

--- main.py
+++ main.py
@@ -1,6 +1,36 @@
-role: str = 'admin'
+# Проект - менеджер задач
 
-match role:
-    case 'admin':
-        print('asdf')
-    case _: pass
+def main():
+    print('Привет! Это менеджер задач!')
+    print('help - список команд')
+
+    while True:
+        try:
+            user_input = input('> ').strip()
+            parts = user_input.split()
+            cmd, *args = parts
+
+            match cmd:
+                case 'help':
+                    print('help - список команд')
+                case 'add':
+                    pass
+                case 'remove':
+                    pass
+                case 'edit':
+                    pass
+                case 'tags':
+                    pass
+                case 'exit':
+                    print('Программа завершена')
+                    break
+                case _:
+                    print('Неизвестная команда')
+        except KeyboardInterrupt:
+            print('\nПрограмма завершена...')
+            break
+        except Exception as e:
+            print("Ошибка:", e)
+
+
+main()
